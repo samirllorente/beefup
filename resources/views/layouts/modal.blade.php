07c3e2527c208803d55e8a21bbce93a17db6e479
Samir Llorente <samir_llorente@outlook.com>
2018-02-22 22:46:47 -0500

    Modal emergent

--- resources/views/layouts/modal.blade.php
+++ resources/views/layouts/modal.blade.php
@@ -4,10 +4,10 @@
 		@method('DELETE')
 		<div class="modal-header">
 		    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-		    <h5 id="myModalLabel">Eliminar materia</h5>
+		    <h5 id="myModalLabel">{{ $title }}</h5>
 		</div>
 		<div class="modal-body">
-		    <h5 style="text-align: center">¿Desea realmente eliminar esta materia?</h5>
+		    <h5 style="text-align: center">{{ $slot }}</h5>
 		</div>
 		<div class="modal-footer">
 		    <button class="btn btn-default" data-dismiss="modal" aria-hidden="true">Cancelar</button>
@@ -19,7 +19,6 @@
 
 <script type="text/javascript">
     $('a').click(function(event) {
-    	console.log("puto");
         var data_url = $(this).attr('data-url');
         $('#modal-emergent form').attr('action', data_url);
     });
