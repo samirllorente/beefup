88ac58b7b4d2b609b5be2273a54df3e5dccb17a6
Samir Llorente <samir_llorente@outlook.com>
2018-03-27 22:37:06 -0500

    Add token in the meta section and separate the script in a separate file

--- resources/views/reading/show.blade.php
+++ resources/views/reading/show.blade.php
@@ -2,11 +2,21 @@
 
 <?php $isStudent=Auth::user()->hasAnyRole('student'); ?>
 
+@section('meta')
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+@endsection
+
 @section('content')
 	@parent
 	<div class="row">
 		<div class="col-xs-12 {{ $isStudent?'col-sm-8':'' }}">
 			@component('layouts.panel',['title'=>$reading->title])
+				@if($isStudent)
+					@slot('options')
+						<li><a href="#SaveStatus" id="SaveStatus">Guardar resaltadas</a></li>
+						<li><a href="#LoadStatus" id="LoadStatus">Recuperar resaltadas</a></li>
+				    @endslot
+				@endif
 				<div id='TextHighlighter'>
 					{!! $reading->content !!}
 				</div>
@@ -17,6 +27,9 @@
 			@component('layouts.panel')
 				@slot('title')
 			        Palabras desconocidas
+			    @endslot
+			    @slot('options')
+			    	<li><a href="#Remove" id="Remove">Eliminar todas</a></li>
 			    @endslot
 			    <div id="unknown_words"></div>
 			@endcomponent
@@ -39,56 +52,6 @@
 @if($isStudent)
 	@section('script')
 		<script src="{{ asset('vendors/texthighlighter-1.2.0/build/TextHighlighter.min.js') }}"></script>
-		<script type="text/javascript">
-			var words=[];
-			var word_alert = $('#word_alert').clone();
-			var context = document.getElementById("TextHighlighter");
-			var hltr =new TextHighlighter(context,{
-				"onAfterHighlight": function(range, hlts) {
-					if(hlts.length!=0){
-						var word=hlts[0].innerHTML.trim();
-						word_alert.find('strong').html(ucfirst(word));
-						word_alert.removeClass('out');
-						word_alert.addClass('in');
-						if(searchWordIfNotAdd(word)){
-							hltr.removeHighlights(hlts[0]);
-							hltr.find(word,false);
-							alert=word_alert.clone();
-							alert.click(function(){
-								removeWord(word);
-							});
-							$('#unknown_words').append(alert);
-						}
-					}else{
-						return true;
-					}
-				}
-			});
-			//Retorna true si agrega la palabra
-			function searchWordIfNotAdd(word){
-				word=word.toLowerCase().trim();
-				if(words.indexOf(word)==-1){
-					words.push(word);
-					return true;
-				}
-				return false;
-			}
-			//Deseleciona todo, elimina la palabra y vuelve y seleciona
-			function removeWord(word){
-				word=word.toLowerCase().trim();
-				var position=words.indexOf(word);
-				if(position!=-1){
-					words.splice(position,1);
-					hltr.removeHighlights();
-					words.forEach(function(element) {
-					    hltr.find(element,false);
-					});
-				}
-			}
-			//Primera palabra en mayuscula
-			function ucfirst(word){
-				return word.charAt(0).toUpperCase()+word.slice(1).toLowerCase();
-			}
-		</script>
+		<script src="{{ asset('js/textHighlighter.js') }}"></script>
 	@endsection
 @endif
