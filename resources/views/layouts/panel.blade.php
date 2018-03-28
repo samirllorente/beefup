b2148e15054c35f4839fbf607023dab35aba1c8a
Samir Llorente <samir_llorente@outlook.com>
2018-03-27 22:15:53 -0500

    Adding variable for additional options to the panel

--- resources/views/layouts/panel.blade.php
+++ resources/views/layouts/panel.blade.php
@@ -4,15 +4,15 @@
 			<div class="x_title">
 				<h2>{{ $title }}</h2>
 				<ul class="nav navbar-right panel_toolbox">
-					<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-					</li>
-					<!--li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+					<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+					@isset($options)
+					<li class="dropdown">
+						<a href="#Options" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-bars"></i></a>
 						<ul class="dropdown-menu" role="menu">
-							<li><a href="#">Settings 1</a></li>
-							<li><a href="#">Settings 2</a></li>
+							{{ $options }}
 						</ul>
-					</li-->
+					</li>
+					@endisset
 					<li><a class="close-link"><i class="fa fa-close"></i></a></li>
 				</ul>
 			<div class="clearfix"></div>
