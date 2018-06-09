dabfcbfefdfc82d35585227827c7815a3aafee80
Samir Llorente <samir_llorente@outlook.com>
2018-06-09 00:48:52 -0500

    Updating of template Gentelella

--- public/js/custom.js
+++ public/js/custom.js
@@ -70,34 +70,36 @@ var setContentHeight = function () {
 };
 
   $SIDEBAR_MENU.find('a').on('click', function(ev) {
-	  console.log('clicked - sidebar_menu');
-        var $li = $(this).parent();
-
-        if ($li.is('.active')) {
-            $li.removeClass('active active-sm');
-            $('ul:first', $li).slideUp(function() {
-                setContentHeight();
-            });
-        } else {
-            // prevent closing menu if we are on child menu
-            if (!$li.parent().is('.child_menu')) {
-                $SIDEBAR_MENU.find('li').removeClass('active active-sm');
-                $SIDEBAR_MENU.find('li ul').slideUp();
-            }else
-            {
+		var $li = $(this).parent();
+		if ($li.is('.active')) {
+			$li.removeClass('active active-sm');
+			$('ul:first', $li).slideUp(function() {
+				setContentHeight();
+			});
+		}
+		else
+		{
+			// prevent closing menu if we are on child menu
+			if (!$li.parent().is('.child_menu')) {
+				$SIDEBAR_MENU.find('li').removeClass('active active-sm');
+				$SIDEBAR_MENU.find('li ul').slideUp();
+			}else
+			{
 				if ( $BODY.is( ".nav-sm" ) )
 				{
-					$SIDEBAR_MENU.find( "li" ).removeClass( "active active-sm" );
-					$SIDEBAR_MENU.find( "li ul" ).slideUp();
+					if (!$li.parent().is('.child_menu')) {
+						$SIDEBAR_MENU.find('li').removeClass('active active-sm');
+						$SIDEBAR_MENU.find('li ul').slideUp();
+					}
 				}
 			}
-            $li.addClass('active');
+			$li.addClass('active');
 
-            $('ul:first', $li).slideDown(function() {
-                setContentHeight();
-            });
-        }
-    });
+			$('ul:first', $li).slideDown(function() {
+				
+			});
+		}
+	});
 
 // toggle small or large menu 
 $MENU_TOGGLE.on('click', function() {
@@ -114,8 +116,6 @@ $MENU_TOGGLE.on('click', function() {
 	$BODY.toggleClass('nav-md nav-sm');
 
 	setContentHeight();
-
-	$('.dataTable').each ( function () { $(this).dataTable().fnDraw(); });
 });
 
 	// check active menu
